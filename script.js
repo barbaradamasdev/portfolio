05339567f041bc903a802ee31820c044f1076aa1
Barbara Damasceno <barbaradamas.dev@gmail.com>
2023-06-30 17:23:55 -0300

    layout

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu () {
+    document.getElementById('nav-secondary').classList.toggle('active');
+    document.getElementById('close-menu').classList.toggle('active');
+    document.getElementById('line1').classList.toggle('change');
+    document.getElementById('line2').classList.toggle('change');
+    document.getElementById('line3').classList.toggle('change');
+}
