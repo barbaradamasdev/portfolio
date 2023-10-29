09c63a3e4d9d2c77456c8c43219a3871f7edb47e
Bárbara Damasceno <115722516+barbaradamasdev@users.noreply.github.com>
2023-10-29 19:54:56 -0300

    change name "Babi" to "Barbara"

--- script.js
+++ script.js
@@ -184,7 +184,7 @@ let data = {
         navprojects: 'Projetos',
         navcontact:'Contato',
 
-        introdescription1: 'Olá, sou Babi, uma desenvolvedora fullstack com foco em frontend, atualmente moro em Recife, Brasil.',
+        introdescription1: 'Olá, sou Barbara, uma desenvolvedora fullstack com foco em frontend, atualmente moro em Recife, Brasil.',
         introdescription2: 'Desde a adolescência, a tecnologia sempre me fascinou. Inicialmente explorando o mundo do design web, acabei seguindo meu coração e me dedicando à música, tocando em orquestras e ensinando a arte a diversos públicos.',
         introdescription3: "Mais tarde, a vida me guiou para a faculdade de sistemas de informação, onde aprendi algoritmos, bancos de dados e Java. No meio da faculdade, fundei uma empresa de confeitaria, onde desenvolvi habilidades essenciais de ouvir e resolver problemas dos clientes. Em 2022, a vida me chamou de volta para a tecnologia. Desde então, tenho estudado intensamente, apaixonando-me por cada momento dessa jornada. ",
         introbtnprincipal: 'Fala comigo',
@@ -226,7 +226,7 @@ let data = {
         navprojects: 'Projects',
         navcontact:'Contact',
 
-        introdescription1: "Hello, I'm Babi, a fullstack developer with a focus on frontend. based in Recife, Brazil.",
+        introdescription1: "Hello, I'm Barbara, a fullstack developer with a focus on frontend. based in Recife, Brazil.",
         introdescription2: "Technology has always fascinated me since adolescence. Initially exploring the world of web design, I eventually followed my heart and dedicated myself to music, playing in orchestras and teaching the art to diverse audiences. ",        
         introdescription3: "Later on, life led me to pursue a degree in Information Systems, where I learned algorithms, databases, and Java. In the midst of my college journey, I founded a confectionery business, honing essential skills in listening to and solving customers' problems.In 2022, life called me back to technology. Since then, I've been studying intensively and falling in love with every moment of this journey.",
         introbtnprincipal: 'Hire me',
