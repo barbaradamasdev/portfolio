c922448aedc3d628593a3eaa2b5fa21431f456c5
Bárbara Damasceno <115722516+barbaradamasdev@users.noreply.github.com>
2023-10-29 19:51:58 -0300

    change error ""

--- script.js
+++ script.js
@@ -185,8 +185,8 @@ let data = {
         navcontact:'Contato',
 
         introdescription1: 'Olá, sou Babi, uma desenvolvedora fullstack com foco em frontend, atualmente moro em Recife, Brasil.',
-        introdescription2: 'Technology has always fascinated me since adolescence. Initially exploring the world of web design, I eventually followed my heart and dedicated myself to music, playing in orchestras and teaching the art to diverse audiences.',
-        introdescription3: 'Later on, life led me to pursue a degree in Information Systems, where I learned algorithms, databases, and Java. In the midst of my college journey, I founded a confectionery business, honing essential skills in listening to and solving customers' problems.In 2022, life called me back to technology. Since then, I've been studying intensively and falling in love with every moment of this journey.',
+        introdescription2: 'Desde a adolescência, a tecnologia sempre me fascinou. Inicialmente explorando o mundo do design web, acabei seguindo meu coração e me dedicando à música, tocando em orquestras e ensinando a arte a diversos públicos.',
+        introdescription3: "Mais tarde, a vida me guiou para a faculdade de sistemas de informação, onde aprendi algoritmos, bancos de dados e Java. No meio da faculdade, fundei uma empresa de confeitaria, onde desenvolvi habilidades essenciais de ouvir e resolver problemas dos clientes. Em 2022, a vida me chamou de volta para a tecnologia. Desde então, tenho estudado intensamente, apaixonando-me por cada momento dessa jornada. ",
         introbtnprincipal: 'Fala comigo',
         introbtnsecondary: 'Veja meu portfolio',
 
