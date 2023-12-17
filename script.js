1b21f199b0954fbe12ae913a91c2130bd5cd3c64
Bárbara Damasceno <115722516+barbaradamasdev@users.noreply.github.com>
2023-12-17 16:14:42 -0300

    bug: correction in gramatic text

--- script.js
+++ script.js
@@ -211,7 +211,7 @@ let data = {
         navprojects: 'Projects',
         navcontact:'Contact',
 
-        introdescription1: "Hello, I'm Barbara, a fullstack developer with a focus on frontend. based in Recife, Brazil.",
+        introdescription1: "Hello, I'm Barbara, a fullstack developer with a focus on frontend. Based in Recife, Brazil.",
         introdescription2: "Technology has always fascinated me since adolescence. Initially exploring the world of web design, I eventually followed my heart and dedicated myself to music, playing in orchestras and teaching the art to diverse audiences. ",        
         introdescription3: "Later on, life led me to pursue a degree in Information Systems, where I learned algorithms, databases, and Java. In the midst of my college journey, I founded a confectionery business, honing essential skills in listening to and solving customers' problems. In 2022, life called me back to technology. Since then, I've been studying intensively and falling in love with every moment of this journey.",
         introbtnprincipal: 'Hire me',
