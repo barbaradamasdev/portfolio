62dc838bb08115ede3d25f58c3c11bd3fd7b9226
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-01-12 11:32:07 -0300

    create angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { ExperienceComponent } from "./components/experience/experience.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { TopbuttonComponent } from "./components/topbutton/topbutton.component";
+import { CardComponent } from "./components/card/card.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, AboutComponent, ProjectsComponent, ExperienceComponent, ContactComponent, FooterComponent, TopbuttonComponent, CardComponent]
+})
+export class AppComponent {
+  title = 'portfolio';
+}
