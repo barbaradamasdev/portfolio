9f297d3c1a50c9b1640bb45fa2703744f120e96f
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-01-18 18:23:04 -0300

    adjust about text

--- src/app/services/about-text.service.ts
+++ src/app/services/about-text.service.ts
@@ -7,16 +7,16 @@ export class AboutTextService {
   aboutText = [
     {
       Desc1: {
-        pt: 'Olá, sou Barbara, uma desenvolvedora fullstack com foco em frontend, atualmente moro em Recife, Brasil.',
-        en: "Hello, I'm Barbara, a fullstack developer with a focus on frontend. Based in Recife, Brazil.",
+        pt: 'Olá, sou Barbara, desenvolvedora fullstack. Desde a adolescência, a tecnologia sempre me fascinou. Inicialmente explorando o mundo do design web, acabei seguindo meu coração e me dedicando à música, tocando em orquestras e ensinando a arte a diversos públicos.',
+        en: "Hello, I'm Barbara, fullstack developer. Technology has always fascinated me since adolescence. Initially exploring the world of web design, I eventually followed my heart and dedicated myself to music, playing in orchestras and teaching the art to diverse audiences.",
       },
       Desc2: {
-        pt: 'Desde a adolescência, a tecnologia sempre me fascinou. Inicialmente explorando o mundo do design web, acabei seguindo meu coração e me dedicando à música, tocando em orquestras e ensinando a arte a diversos públicos.',
-        en: 'Technology has always fascinated me since adolescence. Initially exploring the world of web design, I eventually followed my heart and dedicated myself to music, playing in orchestras and teaching the art to diverse audiences.',
+        pt: 'Mais tarde, a vida me guiou para a faculdade de sistemas de informação, onde aprendi algoritmos, bancos de dados e Java. No meio da faculdade, fundei uma empresa de confeitaria, onde desenvolvi habilidades essenciais de ouvir e resolver problemas dos clientes.',
+        en: 'Later on, life led me to pursue a degree in Information Systems, where I learned algorithms, databases, and Java. In the midst of my college journey, I founded a confectionery business, honing essential skills in listening to and solving customers\' problems.',
       },
       Desc3: {
-        pt: 'Mais tarde, a vida me guiou para a faculdade de sistemas de informação, onde aprendi algoritmos, bancos de dados e Java. No meio da faculdade, fundei uma empresa de confeitaria, onde desenvolvi habilidades essenciais de ouvir e resolver problemas dos clientes. Em 2022, a vida me chamou de volta para a tecnologia. Desde então, tenho estudado intensamente, apaixonando-me por cada momento dessa jornada.',
-        en: 'Later on, life led me to pursue a degree in Information Systems, where I learned algorithms, databases, and Java. In the midst of my college journey, I founded a confectionery business, honing essential skills in listening to and solving customers\' problems. In 2022, life called me back to technology. Since then, I\'ve been studying intensively and falling in love with every moment of this journey.',
+        pt: ' Em 2022, a vida me chamou de volta para a tecnologia. Desde então, tenho estudado intensamente, apaixonando-me por cada momento dessa jornada.',
+        en: ' In 2022, life called me back to technology. Since then, I\'ve been studying intensively and falling in love with every moment of this journey.',
       },
       ButtonHire: {
         pt: 'Fala comigo',
