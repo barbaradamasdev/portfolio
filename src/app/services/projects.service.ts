4acc3710a0f6a03cad7561dc6c4933457f75efc8
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-05-06 20:15:22 -0300

    add projects back end c#

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -61,6 +61,28 @@ export class ProjectsService {
       Technologies: ['Typescript', 'Angular', 'API', 'HTML', 'CSS']
     },
     {
+      Title: 'Capibank - Fintech',
+      Image: '../../../assets/projects/projeto capibank.jpg',
+      Github: 'https://github.com/barbaradamasdev/capibank-hackadev',
+      Live: 'https://capibank.vercel.app/',
+      Description: {
+        pt: 'Como membro da equipe responsável pelo desenvolvimento do projeto Capibank, uma fintech criada pela equipe Sharp Troopers como parte do Bootcamp SharpCoders, desempenhei diversos papéis-chave. Além de contribuir com minhas habilidades em Angular e C#, fui responsável pela criação das telas do frontend, atuando como Product Owner, líder técnica e realizando revisão de código. Nosso objetivo era simular um ambiente de desenvolvimento do mundo real usando a metodologia Scrum, e trabalhamos arduamente para criar uma solução financeira completa, combinando nossas habilidades e experiências.',
+        en: 'As a member of the team responsible for the development of the Capibank project, a fintech created by the Sharp Troopers team as part of the SharpCoders Bootcamp, I played several key roles. In addition to contributing my skills in Angular and C#, I was responsible for creating the frontend screens, acting as Product Owner, technical lead, and conducting code reviews. Our goal was to simulate a real-world development environment using the Scrum methodology, and we worked diligently to create a complete financial solution, combining our skills and experiences.'
+      },
+      Technologies: ['Typescript', 'Angular', 'API', 'C#', 'HTML', 'CSS']
+    },
+    {
+      Title: 'ProcessTrack - Monitor de Processos',
+      Image: '../../../assets/projects/projeto processtrack.jpg',
+      Github: 'https://github.com/barbaradamasdev/DesafioMXM',
+      Live: '',
+      Description: {
+        pt: 'Desenvolvi o projeto "Monitor de Processos" como um desafio técnico de seleção. O objetivo era criar um monitor de processos do sistema operacional, listando os processos, os recursos consumidos e utilizando gráficos para ilustrar o processamento. Implementei recursos como loading para indicar o carregamento, uma tabela de processos com ordenação e busca, armazenamento por drives existentes e detalhamento do gráfico com mouse, garantindo também a responsividade do aplicativo.',
+        en: "I developed the 'Process Monitor' project as a technical challenge for selection. The goal was to create a system process monitor, listing processes, resource consumption, and using charts to illustrate processing. I implemented features such as loading indicators, a process table with sorting and search functionality, storage by existing drives, and detailed chart information on mouse hover, ensuring the application's responsiveness."
+      },
+      Technologies: ['Typescript', 'Angular', 'API', 'C#', 'HTML', 'CSS']
+    },
+    {
       Title: 'Pokedex - API Website',
       Image: '../../../assets/projects/projeto pokedex.jpg',
       Github: 'https://github.com/barbaradamasdev/js-pokedex',
