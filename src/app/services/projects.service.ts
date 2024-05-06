7c7aac5970e52c11740aa55e4b55d74c9f406948
Bárbara Damasceno <115722516+barbaradamasdev@users.noreply.github.com>
2024-05-06 16:59:11 -0300

    adiciona pandas barbaros projeto

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -14,7 +14,7 @@ export class ProjectsService {
         pt: 'Projeto Blizzard com o objetivo de criar um clone do site da Blizzard, seguindo o modelo de Gilberto Prado para o BRChallenges. Garanti responsividade para diversas resoluções e destaquei a interatividade com efeitos de hover no menu e imagens dos jogos exclusivos. Além disso, implementei um modal de login para criar experiências mais realista para o usuário.',
         en: 'Blizzard project with the aim of creating a clone of the Blizzard website, following the model by Gilberto Prado for BRChallenges. I ensured responsiveness for various resolutions and emphasized interactivity with hover effects on the menu and images of exclusive games. Additionally, I implemented a login modal, showcasing my ability to create engaging user experiences.',
       },
-      Technologies: ['Typescript', 'Angular', 'HTML5']
+      Technologies: ['Typescript', 'Angular', 'HTML']
     },
     {
       Title: 'MilkBark - Task Manager',
@@ -25,7 +25,18 @@ export class ProjectsService {
         pt: 'Desenvolvimento de um sistema responsivo para gerenciamento de tarefas, aplicando conhecimentos nas linguagens básicas de front end e utilizando localStorage, seguindo diretrizes e wireframes propostos no desafio do curso. Consegui me tornar monitora do curso com a conclusão desse desafio.',
         en: 'Developed a responsive task management system by applying skills in basic front-end languages and utilizing localStorage, following guidelines and wireframes proposed in the course challenge. Achieved the position of course monitor upon successful completion of this project.',
       },
-      Technologies: ['JavaScript', 'CSS', 'HTML5']
+      Technologies: ['JavaScript', 'CSS', 'HTML']
+    },
+    {
+      Title: 'Pandas Bárbaros - Confeitaria',
+      Image: '../../../assets/projects/projeto pandas barbaros.jpg',
+      /*Github: 'https://github.com/barbaradamasdev/to-do-list',*/
+      Live: 'https://www.pandasbarbaros.com.br/',
+      Description: {
+        pt: 'Desenvolvi o site da confeitaria Pandas Barbaros utilizando WordPress, onde foquei no design, na interface do usuário e na experiência do usuário. Criei um layout atrativo e funcional para exibir os produtos da confeitaria, além de implementar funcionalidades como um sistema de pedidos online e um blog para compartilhar receitas e dicas de culinária.',
+        en: "I developed the website for Pandas Barbaros Bakery using WordPress, focusing on design, user interface, and user experience. I created an attractive and functional layout to showcase the bakery's products, as well as implementing features such as an online ordering system and a blog to share recipes and cooking tips."
+      },
+      Technologies: ['Wordpress']
     },
     {
       Title: 'Space Website',
@@ -36,7 +47,7 @@ export class ProjectsService {
         pt: 'Desenvolvi um site espacial, usando HTML, CSS e JavaScript vanilla para maior controle das ferramentas puras. Esse é um desafio do FrontEnd Mentor. Adoto sempre Mobile First e utilizei Flexbox, implementei animações e um menu "sanduíche" para facilitar a navegação na versão mobile. Priorizei a acessibilidade, enfatizando a hierarquia para garantir uma experiência inclusiva e acessível a todos os usuários.',
         en: 'I developed a space-themed website using HTML, CSS, and vanilla JavaScript for greater control over pure tools. This is a challenge from FrontEnd Mentor. I always adopt a Mobile First approach, utilizing Flexbox, implementing animations, and a "sandwich" menu to enhance navigation on the mobile version. I prioritized accessibility, emphasizing hierarchy to ensure an inclusive and accessible experience for all users.',
       },
-      Technologies: ['JavaScript', 'CSS', 'HTML5']
+      Technologies: ['JavaScript', 'CSS', 'HTML']
     },
     {
       Title: 'TecaFocus - Movie Database',
@@ -47,7 +58,7 @@ export class ProjectsService {
         pt: 'Curadoria para entusiastas de filmes e séries. Desenvolvido utilizando Angular v.17, o site integra uma API da OMDB para fornecer aos usuários recomendações personalizadas, avaliações e informações detalhadas sobre filmes e séries. Possui design responsivo para acessibilidade em diversos dispositivos.',
         en: 'Curated for film and series enthusiasts, this project was developed using Angular v.17. The site integrates the OMDB API to provide users with personalized recommendations, reviews, and detailed information about movies and series. It features responsive design for accessibility across various devices.',
       },
-      Technologies: ['Typescript', 'Angular', 'API', 'Html', 'CSS']
+      Technologies: ['Typescript', 'Angular', 'API', 'HTML', 'CSS']
     },
     {
       Title: 'Pokedex - API Website',
@@ -58,7 +69,7 @@ export class ProjectsService {
         pt: 'Desenvolvi uma biblioteca de Pokémon utilizando a API da Pokédex, empregando apenas HTML, CSS e JavaScript puro, sem a utilização de bibliotecas. Incluí uma barra de pesquisa para maior comodidade dos usuários. Dado o caráter comum do projeto, priorizei um estilo moderno, com cores suaves na estética.',
         en: 'A Pokémon library using the Pokédex API, using only HTML, CSS, and pure JavaScript without any libraries. I incorporated a search bar for user convenience. Given the commonplace nature of the project, I prioritized a modern aesthetic with gentle colors in the styling.',
       },
-      Technologies:  ['JavaScript', 'CSS', 'HTML5']
+      Technologies:  ['JavaScript', 'CSS', 'HTML', 'API']
     },
     {
       Title: 'Tattoo Portfolio',
@@ -69,7 +80,7 @@ export class ProjectsService {
         pt: 'Criei um portfólio para um tatuador, empregando HTML, CSS e JavaScript puro para um controle mais preciso. O design adota Grid e Flexbox, garantindo responsividade e seguindo a abordagem "mobile first". Implementei um menu "sanduíche" para facilitar a navegação em dispositivos móveis. A escolha por um site estático, sem bibliotecas, visa proporcionar um maior domínio nas bases. O site foca nas imagens afim de mostrar o trabalho do artista.',
         en: 'I crafted a portfolio for a tattoo artist, utilizing HTML, CSS, and pure JavaScript for precise control. The design incorporates Grid and Flexbox, ensuring responsiveness and adhering to the "mobile-first" approach. I implemented a "sandwich" menu to enhance navigation on mobile devices. Opting for a static site without libraries aims to provide a deeper mastery of the fundamentals. The website emphasizes images to showcase the artist`s work.',
       },
-      Technologies:  ['JavaScript', 'CSS', 'HTML5']
+      Technologies:  ['JavaScript', 'CSS', 'HTML']
     },
     {
       Title: 'Portfolio',
@@ -80,7 +91,7 @@ export class ProjectsService {
         pt: 'Desenvolvi meu próprio portfólio seguindo a abordagem "mobile first" com foco em responsividade. Incluí um darkmode e a capacidade de alterar o idioma do site, além de animações e interações para proporcionar uma experiência visualmente agradável.',
         en: 'I crafted my own portfolio following a "mobile-first" approach with a focus on responsiveness. I included a dark mode, the ability to change the site`s language, along with animations and interactions to provide a visually pleasing experience.',
       },
-      Technologies:  ['Typescript', 'Angular', 'Html', 'CSS']
+      Technologies:  ['Typescript', 'Angular', 'HTML', 'CSS']
     }
   ]
 
